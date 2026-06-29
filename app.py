42dbe0c9d4ded42f2c4ef95135cfb2311e1c560c
pugal202 <pugalshanmugam0@gmail.com>
2026-06-29 19:38:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,15 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 # PostgreSQL
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+database_url = os.getenv("DATABASE_URL")
+
+if database_url:
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    if "sslmode=" not in database_url:
+        database_url += "?sslmode=require"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 
 # Upload Configuration
 app.config["UPLOAD_FOLDER"] = os.path.join("static", "uploads")
